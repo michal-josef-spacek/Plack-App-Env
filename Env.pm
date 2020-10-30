dfaadc959042ac0537b7e1ac4f25acddd5466dbe
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-10-30 14:19:38 +0100

    Prepare for 0.05 version

--- Env.pm
+++ Env.pm
@@ -6,7 +6,7 @@ use warnings;
 
 use Data::Printer;
 
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 sub call {
 	my ($self, $env) = @_;
@@ -137,6 +137,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.04
+0.05
 
 =cut
