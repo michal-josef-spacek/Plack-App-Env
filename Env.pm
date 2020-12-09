1e396356ced2a06281efb7a38d5695333f56e1c1
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-12-09 18:43:17 +0100

    Fix comment in doc example

--- Env.pm
+++ Env.pm
@@ -75,7 +75,7 @@ Returns Plack::Component object.
  use Plack::App::Env;
  use Plack::Runner;
 
- # Run application with one PYX file.
+ # Run application.
  my $app = Plack::App::Env->new->to_app;
  Plack::Runner->new->run($app);
 
